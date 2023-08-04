b228ba8a0a35ab405194f2b6d1296c1854c30218
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2023-08-04 12:17:03 +0200

    Removed unused ECR repo

--- infrastructure/base/modules/ecr/main.tf
+++ infrastructure/base/modules/ecr/main.tf
@@ -2,53 +2,6 @@
 # Module to build the ECR repository
 ##
 
-resource "aws_ecr_repository" "ecr" {
-  name                 = "${var.project}-${var.environment}"
-  image_tag_mutability = var.image_mutability
-  tags                 = var.tags
-
-  encryption_configuration {
-    encryption_type = var.encrypt_type
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "default_policy" {
-  repository = aws_ecr_repository.ecr.name
-  policy     = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Keep last n cms images",
-            "selection": {
-                "tagStatus": "tagged",
-                "tagPrefixList": ["cms"],
-                "countType": "imageCountMoreThan",
-                "countNumber": 3
-            },
-            "action": {
-                "type": "expire"
-            }
-        },
-        {
-            "rulePriority": 2,
-            "description": "Keep last n client images",
-            "selection": {
-                "tagStatus": "tagged",
-                "tagPrefixList": ["client"],
-                "countType": "imageCountMoreThan",
-                "countNumber": 3
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-}
-
-
 # TODO: must be a cleaner way to do it than to duplicate the resource
 
 resource "aws_ecr_repository" "cms_ecr" {
