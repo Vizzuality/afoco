ac75ee98ed3b6fbd2d1e1394e63928bf6a63894b
anamontiaga <amontiaga@gmail.com>
2024-02-13 14:10:55 +0100

    add pull request template

--- client/src/containers/map/settings/basemaps/index.tsx
+++ client/src/containers/map/settings/basemaps/index.tsx
@@ -1,13 +1,11 @@
 import { BASEMAPS } from '@/constants/basemaps';
 
-import BasemapItem from './item';
-
 const Basemaps = () => {
   return (
     <ul className="grid grid-flow-col gap-4">
       {BASEMAPS.map((b) => (
         <li className="col-span-6" key={b.value}>
-          <BasemapItem {...b} />
+          {/* <BasemapItem {...b} /> */}
         </li>
       ))}
     </ul>
