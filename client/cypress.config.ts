0774ffaa626fd6e368bf5bb7417f2e4329955ee7
mluena <maluenarod@gmail.com>
2024-02-27 10:57:54 +0100

    update next var

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: false,
+    screenshotOnRunFailure: true,
+    video: false,
+    viewportWidth: 1280,
+    viewportHeight: 920,
   },
 });
