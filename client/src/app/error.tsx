83d3faea39bd3346b1bb9c74981482370aed46c7
anamontiaga <amontiaga@gmail.com>
2024-02-27 19:47:30 +0100

    hot fix prettier

--- client/src/app/error.tsx
+++ client/src/app/error.tsx
@@ -3,5 +3,9 @@
 import ErrorContainer from '@/containers/error';
 
 export default function ErrorPage() {
-  <ErrorContainer errorType="500" heading="Internal server error" message="Something went wrong and we are working on to fix the problem. Please, try again later." />
+  <ErrorContainer
+    errorType="500"
+    heading="Internal server error"
+    message="Something went wrong and we are working on to fix the problem. Please, try again later."
+  />;
 }
--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link';
 
-import { Button } from '@/components/ui/button';
 import ErrorContainer from '@/containers/error';
+
+import { Button } from '@/components/ui/button';
 
 export default function NotFound() {
   return (
-    <ErrorContainer errorType="404" heading="Page not found" message="It looks like the links is broken or the page has been removed.">
+    <ErrorContainer
+      errorType="404"
+      heading="Page not found"
+      message="It looks like the links is broken or the page has been removed."
+    >
       <Link href="/projects">
         <Button variant="primary" size="base">
           Go to home page
