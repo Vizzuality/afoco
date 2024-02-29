fda1d21f428de9cabf4e1dc159c493912c4a23e3
anamontiaga <amontiaga@gmail.com>
2024-02-29 18:51:52 +0100

    remove non updated test

--- client/cypress/e2e/map.cy.ts
+++ client/cypress/e2e/map.cy.ts
@@ -1,11 +1,4 @@
 describe('map', () => {
-  it('changes basemap', () => {
-    cy.visit('/');
-    cy.get('button[data-cy="map-settings-button"]').click();
-    cy.get('button[data-cy="basemap-satellite"]').click();
-    // Todo: We would need to check if basemap is in the url
-  });
-
   it('loads', () => {
     cy.visit('/');
     const map = cy.get('#default').wait(8000);
