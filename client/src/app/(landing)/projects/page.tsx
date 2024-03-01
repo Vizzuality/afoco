d4c8722a007b0c2fb7b539855a79439450606fa0
anamontiaga <amontiaga@gmail.com>
2024-03-01 19:36:55 +0100

    tab styles

--- client/src/app/(landing)/projects/page.tsx
+++ client/src/app/(landing)/projects/page.tsx
@@ -9,6 +9,8 @@ import Filters from '@/containers/filters';
 import Panel from '@/containers/panel';
 import ProjectsList from '@/containers/projects/list';
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 export default function Projects() {
   return (
     <Panel>
@@ -17,6 +19,23 @@ export default function Projects() {
 
         <Filters />
         <ProjectsList />
+        <Tabs defaultValue="list">
+          <TabsList className="w-full">
+            <TabsTrigger value="statistics" className="rounded-l-lg">
+              Statistics
+            </TabsTrigger>
+            <TabsTrigger value="list" className="rounded-r-lg">
+              List
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="statistics" className="mt-6 space-y-5">
+            Global Stats
+          </TabsContent>
+          <TabsContent value="list" className="mt-6 space-y-5">
+            <Filters />
+            <ProjectsList />
+          </TabsContent>
+        </Tabs>
       </div>
     </Panel>
   );
--- client/src/components/ui/tabs.tsx
+++ client/src/components/ui/tabs.tsx
@@ -15,7 +15,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      'bg-muted text-muted-foreground inline-flex h-9 items-center justify-center rounded-lg p-1',
+      'bg-muted text-muted-foreground inline-flex h-10 items-center justify-center rounded-lg',
       className
     )}
     {...props}
@@ -30,7 +30,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'ring-offset-background focus-visible:ring-ring data-[state=active]:bg-background data-[state=active]:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow',
+      'ring-offset-background focus-visible:ring-ring inline-flex w-1/2 items-center justify-center whitespace-nowrap border border-yellow-400 px-3 py-2 text-base text-yellow-900 transition-all hover:bg-yellow-600 hover:text-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-yellow-400',
       className
     )}
     {...props}
