c4fe05fa9f124f9a1cab4dd92304ead32475a7a6
anamontiaga <amontiaga@gmail.com>
2024-03-12 15:49:50 +0100

    remove commented code

--- client/src/containers/datasets/layers/projects-geometry/hooks.tsx
+++ client/src/containers/datasets/layers/projects-geometry/hooks.tsx
@@ -32,37 +32,4 @@ export function useLayers({
       },
     },
   ] as LayerProps[];
-  //   {
-  //     id: 'projects_points_shadow',
-  //     type: 'circle',
-  //     paint: {
-  //       'circle-radius': 32,
-  //       'circle-color': '#ccc',
-  //       'circle-blur': 1,
-  //       'circle-opacity': opacity,
-  //     },
-  //     layout: {
-  //       visibility: visibility,
-  //     },
-  //   },
-  //   {
-  //     id: 'projects',
-  //     type: 'circle',
-  //     paint: {
-  //       'circle-stroke-color': '#ffffff',
-  //       'circle-stroke-width': ['case', ['boolean', ['feature-state', 'hover'], false], 3, 7],
-  //       'circle-radius': ['case', ['boolean', ['feature-state', 'hover'], false], 13, 7],
-  //       'circle-color': [
-  //         'case',
-  //         ['boolean', ['feature-state', 'hover'], false],
-  //         '#EFB82A',
-  //         '#176252',
-  //       ],
-  //       'circle-opacity': opacity,
-  //     },
-  //     layout: {
-  //       visibility: visibility,
-  //     },
-  //   },
-  // ] as CircleLayer[];
 }
