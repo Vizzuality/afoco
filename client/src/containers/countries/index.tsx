8c257bb23f0c544a26c025eb15e0276ad93f26c1
anamontiaga <amontiaga@gmail.com>
2024-03-12 13:06:31 +0100

    add script

--- client/src/containers/countries/index.tsx
+++ client/src/containers/countries/index.tsx
@@ -3,6 +3,7 @@
 import CountriesList from '@/containers/countries/list';
 
 export default function Countries() {
+  // const { data, isFetching, isFetched, isError, isPlaceholderData } = useGetCountries();
   return (
     <div className="space-y-5 px-6 py-8">
       <h2 className="text-3xl font-normal">Country Profile</h2>
