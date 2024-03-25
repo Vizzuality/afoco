2d781694e989e012ec7d22cf2c49094f9e428eea
anamontiaga <amontiaga@gmail.com>
2024-03-25 15:39:11 +0100

    fix area_protected_total filter

--- client/src/containers/projects/list.tsx
+++ client/src/containers/projects/list.tsx
@@ -40,7 +40,57 @@ export default function ProjectsList() {
           },
         },
         project_indicator_fields: {
+          // $or: [
+          //   {
+          //     ...(filtersSettings.area_plantation?.includes('>500') && {
+          //       indicator_name: 'area_plantation_total',
+          //       filter_tag: {
+          //         $gt: 500,
+          //       },
+          //     }),
+          //   },
+          //   {
+          //     ...(filtersSettings.area_plantation?.includes('<200') && {
+          //       indicator_name: 'area_plantation_total',
+          //       filter_tag: {
+          //         $lt: 200,
+          //       },
+          //     }),
+          //   },
+          //   {
+          //     ...(filtersSettings.area_plantation?.includes('200-500') && {
+          //       indicator_name: 'area_plantation_total',
+          //       filter_tag: {
+          //         $between: [200, 500],
+          //       },
+          //     }),
+          //   },
+          // ],
           $or: [
+            {
+              ...(filtersSettings.area_protected?.includes('>500') && {
+                indicator_name: 'area_protected_total',
+                filter_tag: {
+                  $gt: 500,
+                },
+              }),
+            },
+            {
+              ...(filtersSettings.area_protected?.includes('<200') && {
+                indicator_name: 'area_protected_total',
+                filter_tag: {
+                  $lt: 200,
+                },
+              }),
+            },
+            {
+              ...(filtersSettings.area_protected?.includes('200-500') && {
+                indicator_name: 'area_protected_total',
+                filter_tag: {
+                  $between: [200, 500],
+                },
+              }),
+            },
             {
               ...(filtersSettings.area_plantation?.includes('>500') && {
                 indicator_name: 'area_plantation_total',
