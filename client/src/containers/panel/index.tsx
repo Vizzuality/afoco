1bde7d3d934988efd89100d19ac2302c83fdba9c
anamontiaga <amontiaga@gmail.com>
2024-04-05 16:28:14 +0200

    hide bg when close panel

--- client/src/containers/panel/index.tsx
+++ client/src/containers/panel/index.tsx
@@ -31,7 +31,8 @@ export default function Panel({ children }: { children: React.ReactNode }) {
         'animate-in slide-in-from-left fade-in rounded-4xl bg-background after:rounded-b-4xl before:rounded-t-4xl absolute bottom-0 top-0 z-10 my-2 flex w-full max-w-[342px] flex-col shadow-md transition-transform duration-700 before:absolute before:left-0 before:top-1 before:h-6 before:w-full before:bg-gradient-to-b before:from-white/100 before:to-white/0 before:content-[""] after:absolute after:bottom-1 after:left-0 after:h-6 after:w-full after:bg-gradient-to-t after:from-white/0 after:to-white/100 after:content-[""] xl:max-w-[400px]':
           true,
         'left-20 translate-x-0 xl:left-[106px]': open,
-        'left-[70px] -translate-x-full xl:left-24': !open,
+        'left-[70px] -translate-x-full rounded-[42px] bg-transparent duration-[2000ms] before:content-[] after:content-[] xl:left-24':
+          !open,
       })}
     >
       <div
