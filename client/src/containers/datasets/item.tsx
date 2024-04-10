156ec4c40ae355c6e4bdbc2ca49c1a0ef1adb9df
anamontiaga <amontiaga@gmail.com>
2024-04-10 18:22:54 +0200

    remove old code

--- client/src/containers/datasets/item.tsx
+++ client/src/containers/datasets/item.tsx
@@ -11,8 +11,6 @@ import type { LayerListResponseDataItem } from '@/types/generated/strapi.schemas
 
 import { useSyncLayers } from '@/hooks/datasets/sync-query';
 
-import { INFO } from '@/containers/datasets/layers';
-
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogClose, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import { Switch } from '@/components/ui/switch';
@@ -32,8 +30,6 @@ export default function DatasetsItem(props: LayerListResponseDataItem) {
 
   if (!props.attributes) return null;
 
-  const Info = INFO[props.id];
-
   return (
     <div className="flex flex-col space-y-1.5 border-b py-2">
       <div className="flex items-center justify-between">
@@ -42,10 +38,9 @@ export default function DatasetsItem(props: LayerListResponseDataItem) {
             checked={layers.some((l) => l.id === props.id)}
             onCheckedChange={handleLayerChange}
           />
-          {/* <h3 className="text-sm">{props.attributes.title}</h3> */}
           <h3 className="text-sm">{props.attributes.name}</h3>
         </div>
-        {Info && (
+        {props.attributes.dialog && (
           <Dialog>
             <DialogTrigger
               className="flex items-center justify-center rounded-full p-2 hover:bg-yellow-50 data-[state=open]:bg-yellow-50"
@@ -60,8 +55,6 @@ export default function DatasetsItem(props: LayerListResponseDataItem) {
                 {props.attributes.name}
               </h3>
               <div className="border-b border-t border-gray-100">
-                {/* <Info /> */}
-
                 {props.attributes.dialog && (
                   <Markdown
                     remarkPlugins={[remarkGfm]}
--- client/src/containers/datasets/layers/index.tsx
+++ client/src/containers/datasets/layers/index.tsx
@@ -1,9 +1,6 @@
 import type { LayerProps, DeckGLLayerProps } from '@/types/layers';
 
 import BiomassDensity from '@/containers/datasets/layers/biomass-density/layer';
-import BiomassDensityInfo from '@/containers/datasets/layers/constants/biomass-density';
-import NetForestCarbonFluxInfo from '@/containers/datasets/layers/constants/net-flux-carbon';
-import SoilCarbonDensityInfo from '@/containers/datasets/layers/constants/soil-carbon-density';
 import { LandDegradationLayer } from '@/containers/datasets/layers/land-degradation/layer';
 import NetForestCarbonFluxLayer from '@/containers/datasets/layers/net-forest-carbon-flux/layer';
 import { ProjectsLayer } from '@/containers/datasets/layers/projects/layer';
@@ -11,19 +8,12 @@ import SoilCarbonDensityLayer from '@/containers/datasets/layers/soil-carbon-den
 import TreeCoverLayer from '@/containers/datasets/layers/tree-cover/layer';
 import TreeCoverLossLayer from '@/containers/datasets/layers/tree-cover-loss/layer';
 
-import LandDegradationInfo from './constants/land-degradation';
-import TreeCoverInfo from './constants/tree-cover';
-
 type DeckGLLayerIndex<T> = {
   [key: number]: React.ComponentType<DeckGLLayerProps<T>>;
 };
 
 type SimpleLayerIndex = {
   [key: number]: React.ComponentType<LayerProps>;
-};
-
-type InfoIndex = {
-  [key: number]: React.ComponentType;
 };
 
 // layers that require DeckGLLayerProps
@@ -38,14 +28,6 @@ const SIMPLE_LAYERS: SimpleLayerIndex = {
   5: ProjectsLayer,
   4: SoilCarbonDensityLayer,
   6: LandDegradationLayer,
-};
-
-export const INFO: InfoIndex = {
-  1: TreeCoverInfo,
-  2: BiomassDensityInfo,
-  3: NetForestCarbonFluxInfo,
-  4: SoilCarbonDensityInfo,
-  6: LandDegradationInfo,
 };
 
 function combineLayers<T>(
